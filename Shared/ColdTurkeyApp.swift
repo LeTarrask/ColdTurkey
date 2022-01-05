11c5f8eaf4157a713a744a36cd241e08570b3afa
leTarrask <tarrask@tarrask.com>
2022-01-05 16:12:32 +0000

    Initial Commit

---
+++ Shared/ColdTurkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColdTurkeyApp.swift
+//  Shared
+//
+//  Created by tarrask on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColdTurkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
