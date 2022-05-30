6b8eba41168035f6790bc97e8bacd3f2022ab40c
leTarrask <tarrask@tarrask.com>
2022-05-30 17:06:47 +0100

    implementing email sending feature

---
+++ Shared/Helpers/Email.swift
@@ -0,0 +1,58 @@
+//
+//  Email.swift
+//  Cold Turkey
+//
+//  Created by Alex Luna on 30/05/2022.
+//
+
+import SwiftUI
+import MessageUI
+
+// TO DO: implement send email https://stackoverflow.com/questions/56784722/swiftui-send-email
+
+struct MailView: UIViewControllerRepresentable {
+    
+    @Environment(\.presentationMode) var presentation
+    @Binding var result: Result<MFMailComposeResult, Error>?
+    
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+        
+        @Binding var presentation: PresentationMode
+        @Binding var result: Result<MFMailComposeResult, Error>?
+        
+        init(presentation: Binding<PresentationMode>,
+             result: Binding<Result<MFMailComposeResult, Error>?>) {
+            _presentation = presentation
+            _result = result
+        }
+        
+        func mailComposeController(_ controller: MFMailComposeViewController,
+                                   didFinishWith result: MFMailComposeResult,
+                                   error: Error?) {
+            defer {
+                $presentation.wrappedValue.dismiss()
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(presentation: presentation,
+                           result: $result)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.mailComposeDelegate = context.coordinator
+        return vc
+    }
+    
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {
+        
+    }
+}
---
+++ Shared/Views/FeedbackView.swift
@@ -0,0 +1,54 @@
+//
+//  FeedbackView.swift
+//  ColdTurkey
+//
+//  Created by tarrask on 30/05/2022.
+//
+
+import SwiftUI
+import MessageUI
+
+struct FeedbackView: View {
+    @State var result: Result<MFMailComposeResult, Error>? = nil
+    @State var isShowingMailView = false
+    
+    @State var name = ""
+    @State var subject = ""
+    @State var message = ""
+    
+    var body: some View {
+        // TO DO: Send an email with your feedback about this app
+        VStack {
+            Form {
+                Section("Your name") {
+                    TextField("Name", text: $name)
+                }
+                
+                Section("Subject line") {
+                    TextField("Subject", text: $subject)
+                }
+                
+                Section("Your message") {
+                    TextEditor(text: $message)
+                }
+            }
+            
+            Button(action: {
+                print("tapped")
+                self.isShowingMailView.toggle()
+            }) {
+                Text("Send email")
+            }
+            .disabled(!MFMailComposeViewController.canSendMail())
+            .sheet(isPresented: $isShowingMailView) {
+                MailView(result: self.$result)
+            }
+        }
+    }
+}
+
+struct FeedbackView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedbackView()
+    }
+}
