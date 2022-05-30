7d95b9275047b517a5b6c05dd57fd57ffee3925b
leTarrask <tarrask@tarrask.com>
2022-05-30 15:53:42 +0100

    upgraded deprecated animation code

--- Shared/Views/Onboarding/OnboardingView.swift
+++ Shared/Views/Onboarding/OnboardingView.swift
@@ -10,39 +10,40 @@ import SwiftUI
 
 struct OnboardingView: View {
     @Environment(\.presentationMode) var presentationMode
-
+    
     @State private var currentPage: OnboardingPage = .welcome
     private let pages: [OnboardingPage]
-
+    
     init(pages: [OnboardingPage]) {
         self.pages = pages
     }
-
+    
     /// Localizable strings
     private let nextLabel = NSLocalizedString("Next", comment: "")
     private let dismissLabel = NSLocalizedString("Dismiss", comment: "")
-
+    
     var body: some View {
         VStack {
             ForEach(pages, id: \.self) { page in
                 if page == currentPage {
-                    page.view(action: showNextPage)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .transition(AnyTransition.asymmetric(
-                                        insertion: .move(edge: .trailing),
-                                        removal: .move(edge: .leading))
-                        )
-                        .animation(.default)
+                    withAnimation(.easeInOut) {
+                        page.view(action: showNextPage)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .transition(AnyTransition.asymmetric(
+                                insertion: .move(edge: .trailing),
+                                removal: .move(edge: .leading))
+                            )
+                    }
                 }
             }
-
+            
             HStack {
                 ForEach(pages, id: \.self) { page in
                     Circle()
                         .frame(width: 10, height: 10)
                 }
             }
-
+            
             Group {
                 if currentPage.shouldShowNextButton {
                     HStack {
@@ -60,21 +61,21 @@ struct OnboardingView: View {
                 }
             }
             .padding(EdgeInsets(top: 0, leading: 50, bottom: 50, trailing: 50))
-            .transition(AnyTransition.asymmetric(
-                            insertion: .move(edge: .trailing),
-                            removal: .move(edge: .leading))
-            )
-            .animation(.easeInOut)
+            .transition(withAnimation(.easeInOut) {
+                AnyTransition.asymmetric(
+                    insertion: .move(edge: .trailing),
+                    removal: .move(edge: .leading))
+            })
         }
         .onAppear {
             self.currentPage = pages.first!
         }
     }
-
+    
     private func dismissOnboarding() {
         presentationMode.wrappedValue.dismiss()
     }
-
+    
     private func showNextPage() {
         guard let currentIndex = pages.firstIndex(of: currentPage), pages.count > currentIndex + 1 else {
             return
