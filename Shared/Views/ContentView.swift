597bb7a55760f9e1b10f4f4d672a67eb447b3ff3
leTarrask <tarrask@tarrask.com>
2022-05-30 16:25:56 +0100

    Created first prototype screen

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -12,30 +12,43 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(manager.passes) { pass in
-                    NavigationLink(destination: ShowCreds(store: pass)) {
-                        VStack {
-                            Text(pass.service)
-                                .font(.headline)
-                            Text(pass.login)
-                                .font(.subheadline)
+            VStack {
+                Text("🚨 You saved these passwords here for a reason. \nAre you really sure you need to go back to these sites? 🤔")
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal, 30)
+                    .foregroundColor(.gray)
+                List {
+                    ForEach(manager.passes) { pass in
+                        NavigationLink(destination: ShowCreds(store: pass)) {
+                            VStack(alignment: .leading) {
+                                Text(pass.service)
+                                    .font(.headline)
+                                Text(pass.login)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
                         }
                     }
                 }
+                .listStyle(InsetGroupedListStyle())
+                .navigationBarItems(trailing:
+                                        NavigationLink(destination: CreatePass( manager: manager)) {
+                    Text("Add Password")
+                })
+            .navigationTitle("Stored Passwords")
             }
-            .navigationBarItems(trailing:
-                                    NavigationLink(destination: CreatePass( manager: manager)) {
-                Text("Add Password")
-            })
-            .navigationTitle("Cold Turkey")
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        let store = PassStore(id: UUID(), service: "Twitter", login: "tarrask", password: "ASODAJS", releaseDate: Date.distantFuture)
-        return ContentView()
+        let manager = PassManager()
+        for i in Range(1...5) {
+            let store = PassStore(id: UUID(), service: "Twitter \(i)", login: "tarrask \(i*5)", password: "ASODAJS", releaseDate: Date.distantFuture)
+            manager.passes.append(store)
+        }
+        return ContentView(manager: manager)
     }
 }
