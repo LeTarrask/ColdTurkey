afc7a04dc5e927c6657c35b7aeaa972b25c95f3a
leTarrask <tarrask@tarrask.com>
2022-07-25 19:39:02 +0100

    intents & privacy

--- Shared/Views/Onboarding/OnboardingPage.swift
+++ Shared/Views/Onboarding/OnboardingPage.swift
@@ -8,47 +8,37 @@
 import SwiftUI
 
 enum OnboardingPage: CaseIterable {
-     case welcome
-     case howTo
-//     case permissions
-     case securityOnboarding
-
-     static let fullOnboarding = OnboardingPage.allCases
-
+    case welcome
+    case howTo
+    case securityOnboarding
+    
+    static let fullOnboarding = OnboardingPage.allCases
+    
     var shouldShowNextButton: Bool {
-         switch self {
-         case .welcome, .howTo:
-             return true
-         default:
-             return false
-         }
-     }
-
+        switch self {
+        case .welcome, .howTo:
+            return true
+        default:
+            return false
+        }
+    }
+    
     var shouldShowDismissButton: Bool {
-         switch self {
-         case .securityOnboarding:
-             return true
-         default:
-             return false
-         }
-     }
-
-     @ViewBuilder
-     func view(action: @escaping () -> Void) -> some View {
+        switch self {
+        case .securityOnboarding:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    @ViewBuilder
+    func view(action: @escaping () -> Void) -> some View {
          switch self {
          case .welcome:
              WelcomeOnboarding()
          case .howTo:
              HowTo()
-//         case .permissions:
-//             VStack {
-//                 Text("We need permissions")
-//
-//                 // This button should only be enabled once permissions are set:
-//                 Button(action: action, label: {
-//                     Text("Continue")
-//                 })
-//             }
          case .securityOnboarding:
              SecurityOnboarding()
          }
